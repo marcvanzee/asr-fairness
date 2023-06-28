d8d80bd07829a9890327c1fcd9b3846e090c9165
Marc van Zee <marcvanzee@gmail.com>
2023-06-28 15:59:05 +0000

    Improve eval loop

--- eval_whisper.py
+++ eval_whisper.py
@@ -49,39 +49,21 @@ _WHISPER_LANGS_SUPPORTED = [
     'yi', 'yo', 'zh']
 
 _COMMONVOICE9_LANGS = [
-    'ab', 'ar', 'as', 'az', 'ba', 'bas', 'be', 'bg', 'bn', 'br', 'ca', 'ckb',
-    'cnh', 'cs', 'cv', 'cy', 'da', 'de', 'dv', 'el', 'en', 'eo', 'es', 'et',
+    'ab', 'ar', 'as', 'az', 'ba', 'bas', 'be', 'bg', 'bn', 'br', 'ca', 
+    'cnh', 'cs', 'cv', 'cy', 'da', 'de', 'dv', 'el', 'eo', 'es', 'et', 'ckb',
     'eu', 'fa', 'fi', 'fr', 'fy-NL', 'ga-IE', 'gl', 'gn', 'ha', 'hi', 'hsb',
     'hu', 'hy-AM', 'ia', 'id', 'ig', 'it', 'ja', 'ka', 'kab', 'kk', 'kmr', 'ky',
     'lg', 'lt', 'lv', 'mdf', 'mhr', 'mk', 'ml', 'mn', 'mr', 'mt', 'myv',
     'nan-tw', 'nl', 'nn-NO', 'or', 'pa-IN', 'pl', 'pt', 'rm-sursilv',
     'rm-vallader', 'ro', 'ru', 'rw', 'sah', 'sat', 'sk', 'sl', 'sr', 'sv-SE',
     'sw', 'ta', 'th', 'tig', 'tok', 'tr', 'tt', 'ug', 'uk', 'ur', 'uz', 'vi',
-    'vot', 'yue', 'zh-CN', 'zh-HK', 'zh-TW'
+    'vot', 'yue', 'zh-CN', 'zh-HK', 'zh-TW', 'en'
 ]
 
 _VOXPOPULI_LANGS = [
-    'lt', 'en', 'de', 'fr', 'es', 'pl', 'it', 'ro', 'hu', 'cs', 'nl', 'fi', 'hr',
-    'sk', 'sl', 'et',
+    'lt', 'de', 'fr', 'es', 'pl', 'it', 'ro', 'hu', 'cs', 'nl', 'fi', 'hr',
+    'sk', 'sl', 'et', 'en'
 ]
-
- 
-def get_batch_size(model_size):
-  return {
-      'tiny': 64,
-      'base': 64,
-      'small': 32,
-      'medium': 16,
-      'large': 16,
-      'large-v2': 8,
-  }[model_size]
-
-
-def get_model(model_size, lang):
-  model_type = model_size
-  if 'large' not in model_size and get_lang(lang) == 'en':
-    model_type += ".en"
-  return whisper.load_model(model_type)
 
 
 def get_sentence_key():
@@ -91,50 +73,50 @@ def get_sentence_key():
   }[FLAGS.dataset]
 
 
-def get_dataset(dataset_name, split, lang, batch_size):
+def get_dataset(dataset_name, split, lang, batch_size=8):
+  # Note: we can use larger batch sizes for smaller models, but batching
+  # the dataset takes a lot of time so we just use the same batch size
+  # everywhere and cache the preprocessed dataset.
+
   def preprocess_audio(e):
     audio = whisper.pad_or_trim(e['audio']['array'].flatten())
     mels = whisper.log_mel_spectrogram(audio)
-    return { 'mels': mels, 'text':  e[get_sentence_key()] }
+    return { 'mels': mels, 'text':  e['sentence'] }
 
   def stack_ds(examples):
     return {
       **{'mels': torch.stack([examples['mels']])},
       **{k: [v] for k, v in examples.items()}
     }
+  
+  # Legacy, remove.
+  dataset_cache_name = dataset_name
+  if dataset_name == 'commonvoice':
+    dataset_cache_name = 'common_voice_9_0'
 
-  # Try retrieving two types of preprocessed datasets: with and without batching.
-  dataset_name.replace('/', '___')
-  db_cache_prefix = os.path.join(FLAGS.db_cache_dir,
-                                 f'{dataset_name}_{lang}_{split}')
-  db_unbatched_cache_path = db_cache_prefix + '_nobatch'
-  db_batched_cache_path = db_cache_prefix + f'_bs_{batch_size}'
+  db_cache_path = os.path.join(FLAGS.db_cache_dir,
+                                 f'{dataset_cache_name}_{lang}_{split}')
 
-  do_batch = True
-
-  if os.path.exists(db_batched_cache_path):
-    print(f'Loading batched db cache from: {db_batched_cache_path}')
-    ds = load_from_disk(db_batched_cache_path)
-    do_batch = False
-  elif os.path.exists(db_unbatched_cache_path):
-    print(f'Loading unbatched db cache from {db_unbatched_cache_path}')
-    ds = load_from_disk(db_unbatched_cache_path)
+  if os.path.exists(db_cache_path):
+    print(f'Loading db cache from: {db_cache_path}')
+    ds = load_from_disk(db_cache_path)
   else:
-    print('Downloading dataset')
-    dataset = {
-      'commonvoice': 'mozilla-foundation/common_voice_9_0',
-      'voxpopuli': 'facebook/voxpopuli'
-    }[dataset_name]
-    ds = load_dataset(dataset, lang, split=split, use_auth_token=True,
-                      num_proc=_NUM_CORES)
-    ds = ds.cast_column("audio", Audio(sampling_rate=16000)).with_format("torch")
-    ds = ds.map(preprocess_audio, num_proc=_NUM_CORES)
-    ds.save_to_disk(db_unbatched_cache_path)
-    
-  if do_batch:
-    print(f'Batching dataset (batch size = {batch_size})')
+    print(db_cache_path + '_nobatch')
+    if os.path.exists(db_cache_path + '_nobatch'):  # Legacy: remove
+      print('Loading legacy preprocessed dataset')
+      ds = load_from_disk(db_cache_path + '_nobatch')
+    else:
+      print('Downloading dataset')
+      dataset = {
+        'commonvoice': 'mozilla-foundation/common_voice_9_0',
+        'voxpopuli': 'facebook/voxpopuli'
+      }[dataset_name]
+      ds = load_dataset(dataset, lang, split=split, use_auth_token=True,
+                        num_proc=_NUM_CORES)
+      ds = ds.cast_column("audio", Audio(sampling_rate=16000)).with_format("torch")
+      ds = ds.map(preprocess_audio, num_proc=_NUM_CORES)
     ds = ds.map(stack_ds, batched=True, batch_size=batch_size)
-    ds.save_to_disk(db_batched_cache_path)
+    ds.save_to_disk(db_cache_path)
   
   return ds
 
@@ -185,24 +167,39 @@ def get_langs_to_eval(dataset_name):
   return langs
 
 
+def get_model_type(model_size, lang):
+  if lang == 'en' and 'large' not in model_size:
+    return model_size + '.en'
+  else:
+    return model_size
+
+
 def main(_):
   langs_to_eval = get_langs_to_eval(FLAGS.dataset)
   print('Evaluating dataset', FLAGS.dataset)
   print(f'Evaluating {len(langs_to_eval)} languages: {langs_to_eval}')
   print(f'Evaluating Whisper models sizes: {FLAGS.model_sizes}')
-  
-  for lang in langs_to_eval:
-    print('=== Language', lang)
-    for model_size in FLAGS.model_sizes:
+
+  # First iterate over model size so we can keep the same model in memory for
+  # all languages -- dataset loading is quite fast once it is cached.
+  prev_model_type = None
+  model = None
+  for model_size in FLAGS.model_sizes:
+    print('=== Evaluating model size', model_size)
+    for lang in langs_to_eval:
+      print('=== Evaluating language', lang)
       result_path = f'./eval_results/whisper_{model_size}_{FLAGS.dataset}_{lang}_{_SPLIT}.tsv'
       if os.path.exists(result_path):
         print(f'Results exist already -- skipping:', result_path)
         continue
-      model = get_model(model_size, lang)
-      batch_size = get_batch_size(model_size)
-      print(f'=== Whisper model {model_size}, batch size = {batch_size}')
 
-      ds = get_dataset(FLAGS.dataset, _SPLIT, lang, batch_size)
+      if (model_type := get_model_type(model_size, lang)) != prev_model_type:
+        # Switch the model if it is different from the previous iteration.
+        print('=== Loading new model', model_type)
+        model = whisper.load_model(model_type)
+        prev_model_type = model_type
+
+      ds = get_dataset(FLAGS.dataset, _SPLIT, lang)
       results = eval(model, ds, lang)
       save_results(result_path, results)
 

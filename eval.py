f10c4fcfbbff66d52c03c88900927d064aa609a3
Marc van Zee <marcvanzee@gmail.com>
2023-06-29 14:37:08 +0000

    Adds final whisper_voxpopuli results and fixes bugs

--- eval.py
+++ eval.py
@@ -12,7 +12,9 @@ import re
 from absl import app
 from absl import flags
 from transformers import Wav2Vec2ForCTC, AutoProcessor
+import pycountry
 import dataclasses
+import functools
 
 
 FLAGS = flags.FLAGS
@@ -46,23 +48,7 @@ _NUM_CORES = 1  # os.cpu_count
 # We always evaluate on the test split.
 _SPLIT = 'test'
 
-
-_WHISPER_LANGS_SUPPORTED = [
-    'af', 'am', 'ar', 'as', 'az', 'ba', 'be', 'bg', 'bn', 'bo', 'br', 'bs',
-    'ca', 'cs', 'cy', 'da', 'de', 'el', 'en', 'es', 'et', 'eu', 'fa', 'fi',
-    'fo', 'fr', 'gl', 'gu', 'ha', 'haw', 'hi', 'hr', 'ht', 'hu', 'hy', 'id',
-    'is', 'it', 'iw', 'ja', 'jw', 'ka', 'kk', 'km', 'kn', 'ko', 'la', 'lb',
-    'ln', 'lo', 'lt', 'lv', 'mg', 'mi', 'mk', 'ml', 'mn', 'mr', 'ms', 'mt',
-    'my', 'ne', 'nl', 'nn', 'no', 'oc', 'pa', 'pl', 'ps', 'pt', 'ro', 'ru',
-    'sa', 'sd', 'si', 'sk', 'sl', 'sn', 'so', 'sq', 'sr', 'su', 'sv', 'sw',
-    'ta', 'te', 'tg', 'th', 'tk', 'tl', 'tr', 'tt', 'uk', 'ur', 'uz', 'vi',
-    'yi', 'yo', 'zh']
-
-_MMS_LANGS_SUPPORTED = [
-  # TODO: https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
-  # MMS uses ISO-639-3 while the datasets use 639-1: convert.
-]
-
+# TODO: extract this from the dataset directly.
 _COMMONVOICE9_LANGS = [
     'ab', 'ar', 'as', 'az', 'ba', 'bas', 'be', 'bg', 'bn', 'br', 'ca', 
     'cnh', 'cs', 'cv', 'cy', 'da', 'de', 'dv', 'el', 'eo', 'es', 'et', 'ckb',
@@ -75,6 +61,7 @@ _COMMONVOICE9_LANGS = [
     'vot', 'yue', 'zh-CN', 'zh-HK', 'zh-TW', 'en'
 ]
 
+# TODO: extract this from the dataset directly.
 _VOXPOPULI_LANGS = [
     'lt', 'de', 'fr', 'es', 'pl', 'it', 'ro', 'hu', 'cs', 'nl', 'fi', 'hr',
     'sk', 'sl', 'et', 'en'
@@ -85,7 +72,7 @@ _VOXPOPULI_LANGS = [
 class WhisperModel:
   model: whisper.model.Whisper
   options: whisper.decoding.DecodingOptions
-  name: str = 'whisper'
+  hf_name: str
 
   @staticmethod
   def preprocess_audio(x):
@@ -95,26 +82,36 @@ class WhisperModel:
     return whisper.log_mel_spectrogram(whisper.pad_or_trim(x))
   
   def decode(self, batch):
-    return self.model.decode(batch['inputs'].to('cuda'), self.options)
+    outputs = self.model.decode(batch['inputs'].to('cuda'), self.options)
+    texts = [output.text for output in outputs]
+    return texts
 
 
 @dataclasses.dataclass
 class MMSModel:
   model: Wav2Vec2ForCTC
-  name: str = 'mms'
+  processor: AutoProcessor
+  hf_name: str
 
   @staticmethod
-  def preprocess_audio(processor, x):
-    return processor(x, sampling_rate=16_000, return_tensors='pt')
+  def preprocess_audio(cls, x):
+    return cls.processor(x, sampling_rate=16_000, return_tensors='pt')
   
   def decode(self, batch):
-    assert len(batch) == 1, 'MMS requires a batch size of 1'
+    assert len(batch['inputs']) == 1, 'MMS requires a batch size of 1'
     inputs = {k: v.to('cuda') for k, v in batch['inputs'][0].items()}
     with torch.no_grad():
       outputs = self.model(**inputs).logits
 
     ids = torch.argmax(outputs, dim=-1)[0]
-    return self.processor.decode(ids)
+    return [self.processor.decode(ids)]
+  
+
+# Note: we store the preprocesss function separately since Arrow datasets cannot
+# serialize the Whisper object, so we cannot use an instance of the
+# WhisperModel class in preprocessing since the cached dataset will not be
+# properly hased.
+ModelState = collections.namedtuple('ModelState', 'model, preprocess_fn')
 
 
 def get_protected_attrs(dataset):
@@ -124,7 +121,7 @@ def get_protected_attrs(dataset):
   }[dataset]
 
 
-def get_batch_size(model):
+def get_batch_size():
   return {
     # Note: we can use larger batch sizes for smaller models, but batching
     # the dataset takes a lot of time so we just use the same batch size
@@ -132,13 +129,14 @@ def get_batch_size(model):
     'whisper': 8,
     # The Huggingface interface to MMS only seems to work for single examples.
     'mms': 1,
-  }[model.name]
+  }[FLAGS.model]
 
 
-def get_batches(dataset, lang, audio_preprocess_fn, batch_size):
+def get_batches(dataset, lang, audio_preprocess_fn):
+  batch_size = get_batch_size()
   def preprocess(hf_name, col_reference, cols_to_keep, cols_to_remove):
     def preprocess_fn(examples):
-      inputs = [audio_preprocess_fn(x['array'].flatten()) for x in examples['audio']]
+      inputs = [audio_preprocess_fn(x=x['array'].flatten()) for x in examples['audio']]
       return { 
           **{'inputs': [inputs],
              'reference_original': [examples[col_reference]]},
@@ -154,7 +152,7 @@ def get_batches(dataset, lang, audio_preprocess_fn, batch_size):
     return ds
     
   db_cache_path = os.path.join(FLAGS.db_cache_dir,
-                                 f'{dataset}_{lang}_{_SPLIT}')
+                                 f'{dataset}_{lang}_{_SPLIT}_bs_{batch_size}')
 
   if os.path.exists(db_cache_path):
     print(f'Loading db cache from: {db_cache_path}')
@@ -180,11 +178,11 @@ def get_batches(dataset, lang, audio_preprocess_fn, batch_size):
   return ds
 
 
-def get_text_normalizer(model, lang):
+def get_text_normalizer(lang):
   normalize_fn = {
     'whisper': EnglishTextNormalizer() if lang == 'en' else BasicTextNormalizer(),
     'mms': lambda x: x
-  }[model.name]
+  }[FLAGS.model]
   return lambda xs: list(map(normalize_fn, xs))
 
 
@@ -198,98 +196,117 @@ def save_results(result_path, results):
     writer.writerows(zip(*[results[key] for key in keys]))
 
 
-def get_lang(code):
-  return re.split('_|-', code)[0] if code else None
+def get_lang_code(code):
+  return code[:code.index('-')] if '-' in code else code
 
 
-def get_langs_to_eval(dataset_name):
-  dataset_langs = {
+def lang_alpha2_to_3(code):
+  # TODO: pycountry seems to miss a lot of alpha_3 languages. Investigate
+  # further.
+  lang = pycountry.countries.get(alpha_2=get_lang_code(code))
+  if lang:
+    lang = lang.alpha_3.lower()
+  return lang
+
+
+def get_langs_to_eval(dataset_name, model_size):
+  ds_langs = {
     'commonvoice': _COMMONVOICE9_LANGS,
     'voxpopuli': _VOXPOPULI_LANGS
   }[dataset_name]
-  langs = [x for x in dataset_langs if get_lang(x) in _WHISPER_LANGS_SUPPORTED]
+  if FLAGS.model == 'whisper':
+    whisper_langs = whisper.tokenizer.LANGUAGES
+    langs = [x for x in ds_langs if get_lang_code(x) in whisper_langs]
+  elif FLAGS.model == 'mms':
+    # We create a temporary processor here just to retrieve all languages.
+    # The processor loads very fast so it doesn't affect performance much.
+    processor = AutoProcessor.from_pretrained('facebook/' + model_size)
+    mms_langs_alpha3 = list(processor.tokenizer.vocab.keys())
+    langs = [lang for lang in ds_langs if lang_alpha2_to_3(lang) in mms_langs_alpha3]
   return langs
-
-
-def get_model_id(model_name, model_size, lang):
-  use_en_model = lang == 'en' and 'large' not in model_size
-  return {
-    'mms': 'facebook/' + model_size,
-    'whisper': model_size + '.en' if use_en_model else model_size
-  }[model_name]
   
 
-def get_model_sizes(model_name):
+def get_model_sizes():
   return {
     'whisper': FLAGS.whisper_model_sizes,
     'mms': FLAGS.mms_model_sizes
-  }[model_name]
+  }[FLAGS.model]
 
 
-def load_model_and_preprocessor(model_name, model_id, lang):
-  print('=== Loading new model', model_name, model_id)
-  if model_name == 'whisper':
+def load_state(hf_name, lang):
+  print('=== Loading new model', hf_name)
+  if FLAGS.model == 'whisper':
     model = WhisperModel(
-      model=whisper.load_model(model_id),
-      options=whisper.DecodingOptions(language=get_lang(lang),
+      hf_name=hf_name,
+      model=whisper.load_model(hf_name),
+      options=whisper.DecodingOptions(language=get_lang_code(lang),
                                       without_timestamps=True))
     preprocessor = WhisperModel.preprocess_audio
-  elif model_name == 'mms':
+  elif FLAGS.model == 'mms':
     model = MMSModel(
-      model=Wav2Vec2ForCTC.from_pretrained(model_id).to('cuda'),
-      processor=AutoProcessor.from_pretrained(model_id)
+      hf_name=hf_name,
+      model=Wav2Vec2ForCTC.from_pretrained(hf_name).to('cuda'),
+      processor=AutoProcessor.from_pretrained(hf_name)
     )
-    preprocessor = MMSModel.preprocess_audio
+    preprocessor = functools.partial(MMSModel.preprocess_audio, cls=model)
 
-  return model, preprocessor
-  
+  return ModelState(model=model, preprocess_fn=preprocessor)
+
+
+def update_model_state(state, model_size, lang):
+  model = state.model
+  use_en_model = lang == 'en' and 'large' not in model_size
+  hf_name = {
+    'mms': 'facebook/' + model_size,
+    'whisper': model_size + '.en' if use_en_model else model_size
+  }[FLAGS.model]
+
+  if not model or model.hf_name != hf_name:
+    state = load_state(hf_name, lang)
+  elif FLAGS.model == 'mms':
+    # Keep the same model in memory and simply switch out the language.
+    lang_alpha3 = lang_alpha2_to_3(lang)
+    model.processor.tokenizer.set_target_lang(lang_alpha3)
+    model.model.load_adapter(lang_alpha3)
+
+  return state
+
 
 def main(_):
   model_name = FLAGS.model
-  model_sizes = get_model_sizes(model_name)
+  model_sizes = get_model_sizes()
   dataset = FLAGS.dataset
-  langs_to_eval = get_langs_to_eval(dataset)
 
   print( ' -------------- Evaluation settings --------------------------------')
   print( '| Model', FLAGS.model)
   print(f'| Models sizes: {model_sizes}')
   print( '| Dataset', dataset)
-  print(f'| {len(langs_to_eval)} languages: {langs_to_eval}')
   print( '|-------------------------------------------------------------------')
 
-  model = None
-  # Keep track of the model id so we can reuse the model it if possible.
-  model_id = None
-  prev_model_id = None
+  state = ModelState(model=None, preprocess_fn=None)
 
   # First iterate over model size so we can keep the same model in memory for
   # all languages -- dataset loading is quite fast once it is cached.
   for model_size in model_sizes:
     print('=== Evaluating model size', model_size)
+    langs_to_eval = get_langs_to_eval(dataset, model_size)
+    print(f'=== Evaluating {len(langs_to_eval)} languages: {langs_to_eval}') 
     for lang in langs_to_eval:
       print('=== Evaluating language', lang)
       result_path = os.path.join(
         FLAGS.eval_results_dir,
         f'{model_name}_{model_size}_{FLAGS.dataset}_{lang}_{_SPLIT}.tsv')
-      print(result_path)
       if os.path.exists(result_path):
         print(f'Results exist already -- skipping:', result_path)
         continue
 
-      model_id = get_model_id(model_name, model_size, lang)
-      if model_id != prev_model_id:
-        # Switch the model if it is different from the previous iteration.
-        model, preprocess_fn = load_model_and_preprocessor(FLAGS.model, model_id, lang)
-        prev_model_id = model_id
-
+      state = update_model_state(state, model_size, lang)
       results = collections.defaultdict(list)
-      normalize = get_text_normalizer(model, lang)
-
-      batch_size = get_batch_size(model)
-      for batch in tqdm(get_batches(dataset, lang, preprocess_fn, batch_size)):
-        outputs = model.decode(batch)
+      normalize = get_text_normalizer(lang)
+      for batch in tqdm(get_batches(dataset, lang, state.preprocess_fn)):
+        texts = state.model.decode(batch)
         results['reference_original'].extend(batch['reference_original'])
-        results['inferred_original'].extend([x.text for x in outputs])
+        results['inferred_original'].extend(texts)
         results['reference'].extend(normalize(results['reference_original']))
         results['inferred'].extend(normalize(results['inferred_original']))
         for key in get_protected_attrs(dataset):

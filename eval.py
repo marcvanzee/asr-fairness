4e53e2e809bc0683f87d5e1ef5837ebbcae298ba
Anna Katrine van Zee <annakatrinevanzee@annas-macbook-air.home>
2023-08-24 12:53:51 +0200

    improves nothing

--- eval.py
+++ eval.py
@@ -10,6 +10,10 @@ print("average",wer(["women group together", "what a weather took a turn"], ["a 
 
 """
 import glob
+import csv
+
+from absl import app
+from absl import flags
 from jiwer import wer, cer, wil
 
 
@@ -23,62 +27,98 @@ from jiwer import wer, cer, wil
 # read db in
 # run fairness evaluations
 
-if dataset == "commonvoice":
-    keys = ["gender", "accent", "age", "locale", "reference", "reference_original", "inferred", "inferred_original"]
-  else: 
-    keys = ["gender", "accent", "reference", "reference_original", "inferred", "inferred_original"]
+FLAGS = flags.FLAGS
 
-def process_voxpopuli(language, data):
+
+flags.DEFINE_enum('dataset', 'commonvoice', ['commonvoice', 'voxpopuli'],
+                  'Name of dataset to run inference on.')
+
+flags.DEFINE_string('inference_results_dir',
+                    './inference_results',
+                    'Directory to store inference results tsv files')
+
+flags.DEFINE_enum('model', 'whisper', ['whisper', 'mms'],
+                  'Model to run inference on.')
+
+flags.DEFINE_list('whisper_model_sizes',
+                  ['tiny', 'base', 'small', 'medium', 'large', 'large-v2'],
+                  'Whisper model sizes to run inference on.')
+
+flags.DEFINE_list('mms_model_sizes',
+                  ['mms-1b-fl102', 'mms-1b-l1107', 'mms-1b-all'],
+                  'MMS model sizes to run inference on.')
+
+
+def eval_inferred(reference, inferred):
+  pass
+
+def process_commonvoice(language, filename):
   file_results = {}
-  for line in data:
-    try:
-      gender, accent, reference, reference_original, inferred, inferred_original, reference, inferred = line.strip().split("\t")
-    except ValueError:
-      print(language, line.strip().split("\t"))
-      continue
 
-    infra_group = f"{gender}"
-    if infra_group == "gender": continue
-    if infra_group not in file_results:
-      file_results[infra_group] = []
-    file_results[infra_group].append(wer(reference, inferred))
+  with open(filename) as f:
+    data = csv.reader(f, delimiter='\t')
+    print(type(csv))
+    for line in data:
+      gender, accent, age, locale, reference, reference_original, inferred, inferred_original = line
+      print(gender, reference)
 
-    if accent != "None":
-      print(f"There is an accent: {line}")
+def process_voxpopuli(language, filename):
+  file_results = {}
+  
+  with open(filename, 'w+') as f:
+    data = csv.reader(f, delimiter='\t')
+    for line in data:
+      gender, accent, age, locale, reference, reference_original, inferred, inferred_original = line
+      print(gender, reference)
 
-  print('language')
-  total_len = 0
-  total_sum = 0
-  for group in file_results:
-    s = sum(file_results[group])
-    l = len(file_results[group])
-    total_sum += s
-    total_len += l
-    print(f"{group} average WER:", s/l)
 
-  print("Total average WER:", total_sum/total_len)
+
+  #   if accent is not "None":
+  #     print(accent)
+  #   minimum_group = f"{gender}"
+  #   if infra_group == "gender": continue
+  #   if infra_group not in file_results:
+  #     file_results[infra_group] = []
+  #   file_results[infra_group].append(wer(reference, inferred))
+
+  #   if accent != "None":
+  #     print(f"There is an accent: {line}")
+
+  # print('language')
+  # total_len = 0
+  # total_sum = 0
+  # for group in file_results:
+  #   s = sum(file_results[group])
+  #   l = len(file_results[group])
+  #   total_sum += s
+  #   total_len += l
+  #   print(f"{group} average WER:", s/l)
+
+  # print("Total average WER:", total_sum/total_len)
   #complete_results[dataset][f"{model}_{modelsize}"][language] = [total]
 
 
-def main():
+def main(_):
 
   complete_results = {"commonvoice": {}, "voxpopuli": {}} # dataset/model_modelsize/language = [totalWER, femWER, mascWER]
 
-  filenames = glob.glob("inference_results/*")
+  filenames = glob.glob("inference_results/*tiny*")
+
   for filename in filenames:
     elements = filename.split("/")[1].split(".")[0].split("_")
     try:
       model, modelsize, dataset, language, datasetsplit = elements
     except ValueError:
       continue
-
     if f"{model}_{modelsize}" not in complete_results[dataset]:
       complete_results[dataset][f"{model}_{modelsize}"] = {}
+
+    if dataset == "commonvoice":
+      process_commonvoice(language, filename)
+
+    else:
+      continue
+      #process_voxpopuli(language, filename)
     
-    data = open(filename)
-
-    if dataset == "voxpopuli":
-      process_voxpopuli(language, data)
-
-
-main()
+if __name__ == '__main__':
+  app.run(main)
